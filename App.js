23203dd51815abb27caf34ad2faa2ede86a70c6e
Leandro Yamanaka <leandroyamanaka@gmail.com>
2021-11-19 16:14:59 -0300

    Ordem numeros corrigido

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
   // Mapeamento de teclas
-  const buttons = ['LIMPAR', 'DEL', '%', '/', 7, 8, 9, "x", 6, 5, 4, '-', 3, 2, 1, '+', 0, '.', '+/-', '=']
+  const buttons = ['LIMPAR', 'DEL', '%', '/', 7, 8, 9, "x", 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '+/-', '=']
 
   const [currentNumber, setCurrentNumber] = useState("")
   const [lastNumber, setLastNumber] = useState("")
@@ -30,6 +30,7 @@ export default function App() {
       case '/': 
         setCurrentNumber((fistNumber / lastNumber).toString())
         return
+     
     }
   }
 
@@ -58,6 +59,8 @@ export default function App() {
           setCurrentNumber(Math.abs(currentNumber));
         }
         return
+      //case '%':
+      //setCurrentNumber(currentNumber / 100);
     }
 
     setCurrentNumber(currentNumber + buttonPressed)
